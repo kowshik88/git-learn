0060881bc2f9206f45966b75c2bbdae7c5fd7e02
kowshik88 <118993097+kowshik88@users.noreply.github.com>
2023-02-23 11:23:19 +0530

    Add files via upload

---
+++ restarent.js
@@ -0,0 +1,85 @@
+let order=document.getElementById("ch-1");
+function clickBtn1()
+{
+    order.textContent="Order Sucessful";
+    order.style.color="white";
+    order.style.backgroundColor="green";
+    window.alert("Congratulation Your Order Confirmed");
+}
+let order1=document.getElementById("ch-2");
+function clickBtn2()
+{
+    order1.textContent="Order Sucessful";
+    order1.style.color="white";
+    order1.style.backgroundColor="green";
+    window.alert("Congratulation Your Order Confirmed");
+    
+}
+let order2=document.getElementById("ch-3");
+function clickBtn3()
+{
+    order2.textContent="Order Sucessful";
+    order2.style.color="white";
+    order2.style.backgroundColor="green";
+    window.alert("Congratulation Your Order Confirmed");
+}
+let order4=document.getElementById("ch-4");
+function clickBtn4()
+{
+    order4.textContent="Order Sucessful";
+    order4.style.color="white";
+    order4.style.backgroundColor="green";
+    window.alert("Congratulation Your Order Confirmed");
+}
+let order5=document.getElementById("ch-5");
+function clickBtn5()
+{
+    order5.textContent="Order Sucessful";
+    order5.style.color="white";
+    order5.style.backgroundColor="green";
+    window.alert("Congratulation Your Order Confirmed");
+}
+let order6=document.getElementById("ch-6");
+function clickBtn6()
+{
+    order6.textContent="Order Sucessful";
+    order6.style.color="white";
+    order6.style.backgroundColor="green";
+    window.alert("Congratulation Your Order Confirmed");
+}
+let order7=document.getElementById("ch-7");
+function clickBtn7()
+{
+    order7.textContent="Order Sucessful";
+    order7.style.color="white";
+    order7.style.backgroundColor="green";
+    window.alert("Congratulation Your Order Confirmed");
+}
+let order8=document.getElementById("ch-8");
+function clickBtn8()
+{
+    order8.textContent="Order Sucessful";
+    order8.style.color="white";
+    order8.style.backgroundColor="green";
+    window.alert("Congratulation Your Order Confirmed");
+}
+let order9=document.getElementById("ch-9");
+function clickBtn9()
+{
+    order9.textContent="Order Sucessful";
+    order9.style.color="white";
+    order9.style.backgroundColor="green";
+    window.alert("Congratulation Your Order Confirmed");
+}
+let order10=document.getElementById("ch-10");
+function clickBtn10()
+{
+    order10.textContent="Order Sucessful";
+    order10.style.color="white";
+    order10.style.backgroundColor="green";
+    window.alert("Congratulation Your Order Confirmed");
+}
+function bookTable()
+{    
+    window.alert("select your seat");
+}
